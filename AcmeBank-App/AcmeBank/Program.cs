387760a6385b2b6943549f0cf1dbb0001d7c31c4
KawsarHussain <kawsar.hussain.365@gmail.com>
2024-03-14 16:14:51 +0000

    Started with DateInputHandling function

--- AcmeBank-App/AcmeBank/Program.cs
+++ AcmeBank-App/AcmeBank/Program.cs
@@ -15,14 +15,72 @@ namespace AcmeBank
                 Hello and welcome to a new and Exciting Journey with us.
                 I will start by asking for your name.
                 """);
-            string firstName = InputHandling("What is your first name", true);
-            string lastName = InputHandling("what is your last name", true);
-            string otherName = InputHandling("what is your middle name / names", true, true);
+            //string firstName = StringInputHandling("What is your first name", true);
+            //string lastName = StringInputHandling("what is your last name", true);
+            //string otherName = StringInputHandling("what is your middle name / names", true, true);
+
+            int day = DateInputHandling("What day were you born?", "Input in the format DD", 1, 31, 2);
+            int month = DateInputHandling("What month were you born?", "Input in the format MM", 1, 12, 2);
 
         }
 
-        //Method to facilitate inputting of inputs
-        private static string InputHandling(string prompt, bool specialCharacterCheck = false, bool isNullable = false)
+        //Method to facilitate inputs of integers
+
+        private static int DateInputHandling(string prompt, string helpPrompt, int minValue, int maxValue, uint digits)
+        {
+            string input;
+            int inputNumber = -1;
+            bool helpConfirm = false;
+            bool correctInputType;
+
+            if (digits == 0) return -1; // If the digits of input is 0 then it returns a -1, indicating that digits values wasn't put in
+            
+            do
+            {
+                //If the user got the input wrong
+                if (helpConfirm) 
+                    Console.WriteLine($"""
+                        {helpPrompt} AND
+                        Make sure the value is between {minValue}-{maxValue}
+                        """);
+                Console.WriteLine(prompt);
+                input = Console.ReadLine();
+
+                //Checks to see if it can convert input to an integer
+                try
+                {
+                    correctInputType = true;
+                    inputNumber = Int32.Parse(input);
+                }
+                catch (FormatException) //If the input cannot be parsed into string
+                {
+                    Console.Clear();
+                    correctInputType = false;
+                    Console.WriteLine("please input an integer value");
+                    helpConfirm = true;
+                    input = "";
+                }
+
+                if (correctInputType)
+                {
+                    if (input.Length == digits && (inputNumber >= minValue && inputNumber <= maxValue))
+                        Console.Clear(); //Breaking Condition
+                    else
+                    {
+                        input = "";
+                        helpConfirm = true;
+                        Console.Clear();
+                    }
+                }
+            } while (input == "");
+
+            return inputNumber;
+        }
+
+
+
+        //Method to facilitate inputting of string inputs
+        private static string StringInputHandling(string prompt, bool specialCharacterCheck = false, bool isNullable = false)
         {
             string input;
             bool characterValidation = true;
