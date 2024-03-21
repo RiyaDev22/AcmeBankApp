c6262d987ec8aadaa82900d79438a3ec1cc0da25
RiyaDev22 <riyamayor@yahoo.co.uk>
2024-03-21 19:55:46 +0000

    Implemented the basic functionality for the Customer Validation class

--- AcmeBank-App/AcmeBank/Program.cs
+++ AcmeBank-App/AcmeBank/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using AcmeBank.BankAccounts;
 using BankPayments.BankAccounts.DerivedAccounts;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
@@ -11,7 +12,7 @@ namespace AcmeBank
         static void Main(string[] args)
         {
             //Create a new Teller object which displays the login screen once the application starts
-            Teller oTeller = new Teller();            
+            Teller oTeller = new Teller();
 
 
             //Customer newCustomer = CustomerUtilities.CreateCustomer();
@@ -20,7 +21,7 @@ namespace AcmeBank
             //Account account = AccountUtilities.LoadAccountDetails("23455432");
             //account.AccountOptionsLoop(); // this is a place holder for now and just holds the basic shared options
 
-            List < Account > accounts = new List<Account>();
+            List<Account> accounts = new List<Account>();
 
             // Adding ISAAccount, BusinessAccount, and PersonalAccount objects to the list
             accounts.Add(new ISAAccount("12345678", "111111", 2000.00m, "1-Main St-ABC123"));
@@ -50,8 +51,37 @@ namespace AcmeBank
                 AccountUtilities.SaveAccountDetails(i);
             }
 
+            //Initialise a string list which will contains customer's details
+            List<string> slCustomersCsv = new List<string>();
+            List<Customer> clCustomers = new List<Customer>();
+            /****************************************************************************************************************************************
+            * TODO!!!: Populate slCustomersCsv with content from the csv file                                                                       *
+            /****************************************************************************************************************************************/
+            //Populate string list with data
+            slCustomersCsv.Add("Kawsar,Hussain,EMPTY,17/04/2001,E15 5DP,17/04/2018,What is the manufacturer of the first car you owned or drove?,Toyota,11112222,33334444");
+            slCustomersCsv.Add("Tom,Scott,EMPTY,15/06/2001,E13 3FJ,17/06/2018,What is the manufacturer of the first car you owned or drove?,Audi,12344321");
+
+
+            //public Customer(string firstName, string lastName, string otherName, DateOnly dateOfBirth, string postCode, string securityQuestion, string securityAnswer)
+
+            //Populate customer list with data from the string list
+            foreach (string sCustomerDetails in slCustomersCsv)
+            {
+                string[] saCustomerDetails = sCustomerDetails.Split(',');
+                DateOnly doDate = DateOnly.Parse(saCustomerDetails[3]);
+
+                clCustomers.Add(new Customer (saCustomerDetails[0].ToString(),
+                                              saCustomerDetails[1].ToString(),
+                                              saCustomerDetails[2].ToString(),
+                                              doDate,
+                                              saCustomerDetails[4].ToString(),
+                                              saCustomerDetails[6].ToString(),
+                                              saCustomerDetails[7].ToString()));
+            }
+
             //Declare object which will display the customer validation screen
             CustomerValidation oCustomerValidation;
+            Customer oCustomer;
 
             //This while loop will run indefinitely until the user press 'x' to quit - Look at the switch case statement
             while (true)
@@ -74,8 +104,13 @@ namespace AcmeBank
                 switch (sUserInput)
                 {
                     case "1":
-                        //Retrieve customer's details by creating a new CustomerValidation object
+                        //Create a new CustomerValidation object
                         oCustomerValidation = new CustomerValidation();
+                        //Retrieve customer's details using the object
+                        oCustomer = oCustomerValidation.ValidateCustomer(clCustomers);
+                        /****************************************************************************************************************************************
+                        * TODO!!!: Display the correct customer's details                                                                                       *
+                        /****************************************************************************************************************************************/
 
                         //loads customer and then presents options
                         Account account = AccountUtilities.LoadAccountDetails("11112222");
@@ -84,13 +119,22 @@ namespace AcmeBank
                     case "2":
                         //Clear the console
                         Console.Clear();
-                        //Invoke function to create a new customer account
-                        CreateCustomer();
+                        //Invoke function in the Customer Utilities class to create a new customer account
+                        Customer oNewCustomer = CustomerUtilities.CreateCustomer();
+                        /****************************************************************************************************************************************
+                        * TODO!!!: Append the new customer details to the list/csv file                                                                         *
+                        /****************************************************************************************************************************************/
                         break;
                     case "3":
                         //Retrieve customer's details by creating a new CustomerValidation object
                         oCustomerValidation = new CustomerValidation();
-                        //Invoke function in the Customer class to remove the customer account
+                        //Retrieve customer's details using the object
+                        oCustomer = oCustomerValidation.ValidateCustomer(clCustomers);
+                        //Invoke function in the Customer Utilities class to remove the customer account
+                        CustomerUtilities.RemoveCustomerDetails(oCustomer);
+                        /****************************************************************************************************************************************
+                        * TODO!!!: Remove the customer details from the list/csv file                                                                           *
+                        /****************************************************************************************************************************************/
                         break;
                     case "*":
                         //Log teller out
@@ -122,18 +166,11 @@ namespace AcmeBank
                         break;
                 }
             }
-
-            /*MOVED THIS PART OF THE CODE TO THE SWITCH CASE STATEMENT ABOVE*/
-            /*//loads customer and then presents options
-            Account account = AccountUtilities.LoadAccountDetails("11112222");
-            account.AccountOptionsLoop(); // this is a place holder for now and just holds the basic shared options*/
         }
 
-            //DateOnly dob = new DateOnly(2001, 4, 17);
-            //Customer kawsar = CustomerUtilities.LoadCustomerDetails("Kawsar", "Hussain", "", dob, "E15 5DP");
+        //DateOnly dob = new DateOnly(2001, 4, 17);
+        //Customer kawsar = CustomerUtilities.LoadCustomerDetails("Kawsar", "Hussain", "", dob, "E15 5DP");
 
-            //CustomerUtilities.RemoveCustomerDetails(kawsar);
-        }        
-
+        //CustomerUtilities.RemoveCustomerDetails(kawsar);
     }
 }
