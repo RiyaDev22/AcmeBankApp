1aa581d2996f401a2657a9ee439ba5f59ca43857
Ted-Singleton <tedsingleton6@gmail.com>
2024-03-21 17:02:55 +0000

    Added comments I forgot to save in the method

--- AcmeBank-App/AcmeBank/InputUtilities.cs
+++ AcmeBank-App/AcmeBank/InputUtilities.cs
@@ -6,14 +6,18 @@
         //we need a method that replaces the usual readlines with one that ensures that input happens within a minute
         public static string? GetInputWithinTimeLimit()
         {
+            //initialise input
             string? input = null;
+            //we create a task to read the input
             var task = Task.Run(() => input = Console.ReadLine());
-            if (task.Wait(TimeSpan.FromMinutes(0.25)))
+            //if this task takes less than 2 minutes, we just return the input
+            if (task.Wait(TimeSpan.FromMinutes(2)))
             {
                 return input;
             }
             else
             {
+                //otherwise, we will show an error message
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("""
@@ -22,7 +26,10 @@
                     Please press Return and start over.
                     """);
                 Console.ResetColor();
+
+                //since security is now a concern, we will exit the program
                 Environment.Exit(1);
+                //we still need a return statement for the method, even though it will never be reached
                 return null;
             }
         }
